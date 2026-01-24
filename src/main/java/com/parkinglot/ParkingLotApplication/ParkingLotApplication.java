9d5db75c2c4c30234c1f16b276abbffb2f41063f
31UTKARSH07 <3107utkarshpathak@gmail.com>
2026-01-25 02:47:28 +0530

    creted userModel and UserRepository

--- src/main/java/com/parkinglot/ParkingLotApplication/ParkingLotApplication.java
+++ src/main/java/com/parkinglot/ParkingLotApplication/ParkingLotApplication.java
@@ -6,17 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.parkinglot.ParkingLotApplication.repository")
 public class ParkingLotApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(),entry.getValue())
-        );
 		SpringApplication.run(ParkingLotApplication.class, args);
 	}
 }
